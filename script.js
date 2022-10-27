a6d9758cd1e734dd3418b209aec6dce704276a1b
Tadas <tadasmasnauskas@gmail.com>
2022-10-27 09:30:31 +0300

    added class constructor

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+//ES6
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    if (this.budget >= 100000000) {
+      console.log(`${this.title} is a very expensive movie`);
+    } else {
+      console.log(`${this.title} is not an expensive movie`);
+    }
+  }
+}
+
+const expensiveMovie = new Movie(
+  "Blockbuster 9000",
+  "Mr. Best Director",
+  10000000000
+);
+
+const cheapMovie = new Movie("B grade Movie", "Random Guy", 50000);
